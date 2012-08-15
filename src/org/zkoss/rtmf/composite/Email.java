132c85bebbb2a1e1a889a089a324eee4a2e084e7
MontyPan <montypan@potix.com>
2012-08-15 10:43:32 +0800

    Add composite example, for http://www.zkoss.org/forum/listComment/20357

---
+++ src/org/zkoss/rtmf/composite/Email.java
@@ -0,0 +1,32 @@
+package org.zkoss.rtmf.composite;
+
+import org.zkoss.zk.ui.IdSpace;
+import org.zkoss.zul.Textbox;
+
+@SuppressWarnings("serial")
+public class Email extends Textbox implements IdSpace{
+	private String category;
+	private String element;
+	
+	public Email() {
+		System.out.println("constructor");
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		System.out.println("set category : "+category);
+		this.category = category;
+	}
+
+	public String getElement() {
+		return element;
+	}
+
+	public void setElement(String element) {
+		System.out.println("set element : "+element);
+		this.element = element;
+	}
+}
